62af79707f0fc8bd41577f15016d1d9e42b7bb1a
Lucianamartins <marialucianasantosalves@gmail.com>
2022-02-17 10:14:30 -0300

    Create project

---
+++ Jogo.RPG/Entities/Wizard.cs
@@ -0,0 +1,33 @@
+﻿namespace Jogo.RPG.Entities
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string Name, int Level, string HeroType)
+        {
+
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+
+
+        }
+        public override string Attack()
+        {
+            return this.Name + "Lançou  sua magia";
+        }
+
+        public string Attack(int Bonus)
+        {
+            if (Bonus > 6)
+            {
+                return this.Name + "Lançou com bonus de ataque " + Bonus;
+            }
+            else
+            {
+
+                return this.Name + "Lançou com força fraca " + Bonus;
+            }
+
+        }
+    }
+}
---
+++ Jogo.RPG/Program.cs
@@ -0,0 +1,24 @@
+﻿using Jogo.RPG.Entities;
+using System;
+
+namespace Jogo.RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Knight arus = new Knight ("Arus", 23, "Knight");
+          Wizard wizard = new Wizard("Rainha dos magos ", 90, "Knight");
+          Ninja ninja = new Ninja("Ninja", 70, "Ninja");
+
+            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(wizard.Attack(7));
+           
+
+
+
+
+        }
+    }
+}
+ 
